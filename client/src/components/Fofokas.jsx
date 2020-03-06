25824af68023cecbdb15c84e7704205a568ae577
diegocamy <diegocamyy@gmail.com>
2020-03-06 13:20:08 -0300

    fix en el sort de las noticias

--- client/src/components/Fofokas.jsx
+++ client/src/components/Fofokas.jsx
@@ -51,16 +51,13 @@ const Fofokas = () => {
 
   const mostrarFofocas = fofocas => {
     //guardar en localStore las fofocas
-    console.log(fofocas);
-    const fofo = [...fofocas];
-    console.log(fofo);
-    fofo.sort(() => Math.random() - 0.5); //pseudo-random sort en las fofocas;
+    // fofo.sort(() => Math.random() - 0.5); //pseudo-random sort en las fofocas;
     let ultimasFofocas = {};
     ultimasFofocas.hora = new Date().getTime();
-    ultimasFofocas.fofocas = fofo;
+    ultimasFofocas.fofocas = fofocas;
     localStorage.setItem('ultimasFofocas', JSON.stringify(ultimasFofocas));
 
-    return fofo.map((f, index) => {
+    return fofocas.map((f, index) => {
       if (f.titulo && f.imagen) {
         return <FofoCard key={index} fofoca={f} />;
       } else {
